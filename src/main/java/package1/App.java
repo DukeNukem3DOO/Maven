b2bc6a1eeecc08e4aaefdfde7b79e01941cc22d3
DukeNukem3DOO <zupal@mail.ru>
2025-02-02 17:52:28 +0300

    new dependency

--- src/main/java/package1/App.java
+++ src/main/java/package1/App.java
@@ -1,10 +1,11 @@
 package package1;
-
+import org.apache.commons.lang3.StringUtils;
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String s = "Hello world";
+        System.out.println(StringUtils.reverse(s));
     }
 }
